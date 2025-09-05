4fc926b4cebeae5bbf46eabfd2b901532e81f163
Shithij M Salian <shithijms23@gmail.com>
2025-09-05 10:31:33 +0530

    modifief home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner' // <-- Import Banner
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedSection />
       <Banner /> {/* <-- Add Banner here */}
+      <Footer />
     </>
   )
 }
