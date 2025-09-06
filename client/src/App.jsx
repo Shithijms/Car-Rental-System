4ce3777ba44d70ea47867763672b44c545aaf158
Shithij M Salian <shithijms23@gmail.com>
2025-09-06 16:53:24 +0530

    Frontend completed with bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,37 @@ import Home from "./pages/Home";
 import Cars from "./pages/Cars";
 import CarDetails from "./pages/CarDetails";
 import MyBooking from "./pages/MyBooking";
+import Footer from "./components/Footer";
+import Layout from "./pages/owner/Layout"; 
+import Dashboard from "./pages/owner/Dashboard";
+import AddCar from "./pages/owner/AddCar";
+import ManageCars from "./pages/owner/ManageCars";
+import ManageBookings from "./pages/owner/ManageBookings";
 
 const App = () => {
-const [showLogin, setShowLogin] = useState(false)
-const isOwnerPath = useLocation().pathname.startsWith("/owner");
-
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
 
   return (
     <>
-        <Navbar setShowLogin={setShowLogin} />
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/cars" element={<Cars/>}/>
-          <Route path="/car-details/:id" element={<CarDetails />} />
-          <Route path="/my-bookings" element={<MyBooking/>}/>
-        </Routes>
+    {showLogin && <Login setShowLogin={setShowLogin} />}
+    
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/my-bookings" element={<MyBooking />} />
+        <Route path="/owner/*" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
+        </Route>
+      </Routes>
+      {!isOwnerPath && <Footer />}
     </>
   )
 }
+
 export default App;
